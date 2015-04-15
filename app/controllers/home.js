576b31cede85b5d58a4bacdd228eb682af1ac482
Erick Brower <cerickbrower@gmail.com>
2015-04-15 15:11:42 -0700

    More complete styles, placeholder blog posts

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -8,5 +8,34 @@ module.exports = function(app) {
 };
 
 router.get('/', function(req, res) {
-  res.render('index', {});
+  res.render('index', {
+    posts: [
+      {
+        title: 'Test Post',
+        slug: 'test-post',
+        text: "Now that we know who you are, I know who I am. I'm " +
+        "not a mistake! It all makes sense! In a comic, you know how " +
+        "you can tell who the arch-villain's going to be? He's the " +
+        "exact opposite of the hero. And most times they're friends, " +
+        "like you and me! I should've known way back when... You know " +
+        "why, David? Because of the kids. They called me Mr Glass.",
+        state: 'published',
+        createdAt: 'February 21st, 2015',
+        updatedAt: 'February 21st, 2015'
+      },
+      {
+        title: 'Another Test Post',
+        slug: 'another-test-post',
+        text: "Now that we know who you are, I know who I am. I'm " +
+        "not a mistake! It all makes sense! In a comic, you know how " +
+        "you can tell who the arch-villain's going to be? He's the " +
+        "exact opposite of the hero. And most times they're friends, " +
+        "like you and me! I should've known way back when... You know " +
+        "why, David? Because of the kids. They called me Mr Glass.",
+        state: 'published',
+        createdAt: 'January 1st, 2015',
+        updatedAt: 'January 1st, 2015'
+      }
+    ]
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,10 @@
 var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   livereload = require('gulp-livereload'),
-  sass = require('gulp-sass'),
-  browserify = require('browserify'),
-  source = require('vinyl-source-stream'),
-  watchify = require('watchify'),
-  reactify = require('reactify');
+  sass = require('gulp-sass');
 
-
-gulp.task('assets:js', function() {
-  var bundler = browserify({
-    entries: ['./app/assets/js/main.js'],
-    transform: [reactify],
-    debug: true, // Gives us sourcemapping
-    cache: {},
-    packageCache: {},
-    fullPaths: true // Requirement of watchify
-  });
-  var watcher = watchify(bundler);
-
-  return watcher
-    .on('update', function() { // When any files update
-      var updateStart = Date.now();
-      console.log('Updating!');
-      watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('app/assets/js/main.js'))
-        // This is where you add uglifying etc.
-        .pipe(gulp.dest('./public/js/'));
-      console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./public/js/'));
-});
-
-gulp.task('assets:sass', function() {
-  gulp.src('./public/css/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./public/css'))
-    .pipe(livereload());
+gulp.task('assets:bower', function () {
+  gulp.src('./app/assets/bower_components');
 });
 
 gulp.task('assets:watch', function() {
@@ -48,8 +14,8 @@ gulp.task('assets:watch', function() {
 gulp.task('develop', function() {
   livereload.listen();
   nodemon({
-    script: 'app.js',
-    ext: 'js handlebars',
+    script: 'run.js',
+    ext: 'js handlebars'
   }).on('restart', function() {
     setTimeout(function() {
       livereload.changed(__dirname);
